9486d50d8f2c23ced596d4dc834e19eaacbd35b2
rijalBinHusen <jeryzal333@gmail.com>
2024-10-01 07:48:05 +0700

    date placeholder oke

--- src/views/Messages/messages.ts
+++ src/views/Messages/messages.ts
@@ -75,42 +75,43 @@ export class Message {
         }
 
         // replace date of time
-        const currentDate = new Date();
-        const next1stDayOfWeek = currentDate.getDay() > 0 ? 7 - currentDate.getDay() : 6 ;
-        const next2ndDayOfWeek = currentDate.getDay() > 1 ? 7 - currentDate.getDay() : 0 ;
-        const next3rdDayOfWeek = currentDate.getDay() > 2 ? 7 - currentDate.getDay() : 1 ;
-        const next4thDayOfWeek = currentDate.getDay() > 3 ? 7 - currentDate.getDay() : 2 ;
-        const next5thDayOfWeek = currentDate.getDay() > 4 ? 7 - currentDate.getDay() : 3 ;
-        const next6thDayOfWeek = currentDate.getDay() > 5 ? 7 - currentDate.getDay() : 4 ;
-        const next7thDayOfWeek = currentDate.getDay() > 6 ? 7 - currentDate.getDay() : 5 ;
+        const currentWeek = new Date();
+        const nextWeekDate = new Date();
+        nextWeekDate.setDate(currentWeek.getDate() + 7);
+        const dayInNumber = currentWeek.getDay();
+        const hoursADayInTime = 1000 * 60  * 60 * 24;
+
         
-        const days = {
-            next1stDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next1stDayOfWeek)),
-            next2ndDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next2ndDayOfWeek)),
-            next3rdDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next3rdDayOfWeek)),
-            next4thDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next4thDayOfWeek)),
-            next5thDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next5thDayOfWeek)),
-            next6thDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next6thDayOfWeek)),
-            next7thDayOfWeek: new Date(new Date().setDate(currentDate.getDate() + next7thDayOfWeek)),
+        if(dayInNumber > 0)  {
+            currentWeek.setDate(currentWeek.getDate() - dayInNumber);
+            nextWeekDate.setDate(nextWeekDate.getDate() - dayInNumber);
         }
+    
+        const next1stDayOfWeek = dayInNumber > 0 ? nextWeekDate : currentWeek ;
+        const next2ndDayOfWeek = dayInNumber > 1 ? new Date(nextWeekDate.getTime() + (1 * hoursADayInTime)) : new Date(currentWeek.getTime() + (1 * hoursADayInTime)) ;
+        const next3rdDayOfWeek = dayInNumber > 2 ? new Date(nextWeekDate.getTime() + (2 * hoursADayInTime)) : new Date(currentWeek.getTime() + (2 * hoursADayInTime)) ;
+        const next4thDayOfWeek = dayInNumber > 3 ? new Date(nextWeekDate.getTime() + (3 * hoursADayInTime)) : new Date(currentWeek.getTime() + (3 * hoursADayInTime)) ;
+        const next5thDayOfWeek = dayInNumber > 4 ? new Date(nextWeekDate.getTime() + (4 * hoursADayInTime)) : new Date(currentWeek.getTime() + (4 * hoursADayInTime)) ;
+        const next6thDayOfWeek = dayInNumber > 5 ? new Date(nextWeekDate.getTime() + (5 * hoursADayInTime)) : new Date(currentWeek.getTime() + (5 * hoursADayInTime)) ;
+        const next7thDayOfWeek = dayInNumber > 6 ? new Date(nextWeekDate.getTime() + (6 * hoursADayInTime)) : new Date(currentWeek.getTime() + (6 * hoursADayInTime)) ;
         
         // replace next1stDayOfWeekAsDay
-        result = result.replace(/{{next1stDayOfWeekAsDay}}/g, days.next1stDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next2ndDayOfWeekAsDay}}/g, days.next2ndDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next3rdDayOfWeekAsDay}}/g, days.next3rdDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next4thDayOfWeekAsDay}}/g, days.next4thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next5thDayOfWeekAsDay}}/g, days.next5thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next6thDayOfWeekAsDay}}/g, days.next6thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
-        result = result.replace(/{{next7thDayOfWeekAsDay}}/g, days.next7thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next1stDayOfWeekAsDay}}/g, next1stDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next2ndDayOfWeekAsDay}}/g, next2ndDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next3rdDayOfWeekAsDay}}/g, next3rdDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next4thDayOfWeekAsDay}}/g, next4thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next5thDayOfWeekAsDay}}/g, next5thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next6thDayOfWeekAsDay}}/g, next6thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
+        result = result.replace(/{{next7thDayOfWeekAsDay}}/g, next7thDayOfWeek.toLocaleString('id-ID', { weekday: 'long' }));
         
         // replace next1stDayOfWeekAsDate
-        result = result.replace(/{{next1stDayOfWeekAsDate}}/g, days.next1stDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next2ndDayOfWeekAsDate}}/g, days.next2ndDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next3rdDayOfWeekAsDate}}/g, days.next3rdDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next4thDayOfWeekAsDate}}/g, days.next4thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next5thDayOfWeekAsDate}}/g, days.next5thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next6thDayOfWeekAsDate}}/g, days.next6thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
-        result = result.replace(/{{next7thDayOfWeekAsDate}}/g, days.next7thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next1stDayOfWeekAsDate}}/g, next1stDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next2ndDayOfWeekAsDate}}/g, next2ndDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next3rdDayOfWeekAsDate}}/g, next3rdDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next4thDayOfWeekAsDate}}/g, next4thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next5thDayOfWeekAsDate}}/g, next5thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next6thDayOfWeekAsDate}}/g, next6thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
+        result = result.replace(/{{next7thDayOfWeekAsDate}}/g, next7thDayOfWeek.toLocaleString('id-ID', { dateStyle: 'long' }));
 
         return {
             message: result,
